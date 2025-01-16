e88cdfc66b93150ec0f2da7f4976baf1f5ad69cd
KUMARSUNNY <ks9107163@gmail.com>
2025-01-17 00:07:02 +0530

    connected to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from './constants';
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
 
+
+dotenv.config({
+    path:'./env'  
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
 import express from 'express';
 const app = express();
 
@@ -10,9 +29,16 @@ const app = express();
    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
    app.on("error",()=>{
     console.log("ERROR:", error);
+    throw error
    })
+
+   app.listen(process.env.PORT, ()=>{
+      console.log(`app is listning on PORT ${process.env.PORT}`)
+   })
+
    } catch(error){
          console.error("ERROR: " ,error)
          throw err
    }
 })()
+   */
